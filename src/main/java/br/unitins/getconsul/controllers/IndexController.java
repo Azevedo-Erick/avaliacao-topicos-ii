d254362cf0cd89a90379ff9e14b41a1181814ac6
Azevedo-Erick <azevedoerick762@gmail.com>
2022-04-03 10:49:00 -0300

    Adição do projeto

---
+++ src/main/java/br/unitins/getconsul/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package br.unitins.getconsul.controllers;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
